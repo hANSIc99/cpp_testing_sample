980e0cf26989d3ae6328150b9c35cea4ac31db6b
Stephan Avenwedde <s.avenwedde@gmail.com>
2021-12-06 10:52:23 +0100

    dev commit

--- Generator/GeneratorTest.cpp
+++ Generator/GeneratorTest.cpp
@@ -1,23 +1,30 @@
 #include <gtest/gtest.h>
 #include <string>
 #include <vector>
+#include <memory>
+#include <numeric>
 
 #include "Generator.h"
 
+
+TEST(GeneratorTest, ReferenceCheck){
+
+  const int NumberOfElements = 10;
+  std::stringstream buffer;
+  
+
+  EXPECT_EQ(
+    std::addressof(buffer),
+    std::addressof(Generator::generate(buffer, NumberOfElements))
+    );
+}
+
 TEST(GeneratorTest, NumberOfElements){
 
+  const int NumberOfElements = 50;
   std::stringstream buffer;
 
-  std::random_device rd;  //Will be used to obtain a seed for the random number engine
-  std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-  std::uniform_int_distribution<> distrib(1, 6);
-  Generator::generate(buffer, 5);
-
-  // Just to view the data during debugging
-  std::string sData; 
-  std::getline(buffer, sData);
-
-
+  Generator::generate(buffer, NumberOfElements);
   // Output vektor
   std::vector<std::string> vNumbers;
   std::string s_no;
@@ -25,5 +32,39 @@ TEST(GeneratorTest, NumberOfElements){
     vNumbers.push_back(s_no);
   }
 
-  EXPECT_EQ(vNumbers.size(), 1);
+  EXPECT_EQ(vNumbers.size(), NumberOfElements);
+}
+
+
+TEST(GeneratorTest, CheckSum){
+
+  const int NumberOfElements = 50;
+  int nChecksum_in = 0;
+  int nChecksum_out = 0;
+  // Input vector
+  std::vector<int> vNumbersRef(NumberOfElements);
+  std::iota(vNumbersRef.begin(), vNumbersRef.end(), 1);
+  
+  // Calculate reference checksum
+  for(const int n : vNumbersRef){
+    nChecksum_in += n;
+  }
+
+  std::stringstream buffer;
+
+  Generator::generate(buffer, NumberOfElements);
+
+  // Output vektor
+  std::vector<int> vNumbersGen;
+  std::string s_no;
+  while(std::getline(buffer, s_no, ' ')) {
+    vNumbersGen.push_back(std::stoi(s_no));
+  }
+
+  // Calculate output checksum
+    for(const int n : vNumbersGen){
+    nChecksum_out += n;
+  }
+
+  EXPECT_EQ(nChecksum_in, nChecksum_out);
 }
